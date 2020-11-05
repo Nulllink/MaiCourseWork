86da0e60d1d34de284da339293a4e8c488e23146
Nulllink <alexanveld@gmail.com>
2020-11-05 21:21:27 +0300

    File mode

--- MaiCourseWork/Tables.Designer.cs
+++ MaiCourseWork/Tables.Designer.cs
@@ -31,8 +31,11 @@
             this.DgwData = new System.Windows.Forms.DataGridView();
             this.MenuStrip = new System.Windows.Forms.MenuStrip();
             this.TsmiWrite = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.TslWrite = new System.Windows.Forms.ToolStripStatusLabel();
             ((System.ComponentModel.ISupportInitialize)(this.DgwData)).BeginInit();
             this.MenuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // DgwData
@@ -60,11 +63,27 @@
             this.TsmiWrite.Text = "Write";
             this.TsmiWrite.Click += new System.EventHandler(this.TsmiWrite_Click);
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TslWrite});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 360);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(500, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // TslWrite
+            // 
+            this.TslWrite.Name = "TslWrite";
+            this.TslWrite.Size = new System.Drawing.Size(0, 17);
+            // 
             // Tables
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 366);
+            this.ClientSize = new System.Drawing.Size(500, 382);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.DgwData);
             this.Controls.Add(this.MenuStrip);
             this.MainMenuStrip = this.MenuStrip;
@@ -73,6 +92,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.DgwData)).EndInit();
             this.MenuStrip.ResumeLayout(false);
             this.MenuStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,5 +104,7 @@
         private System.Windows.Forms.DataGridView DgwData;
         private System.Windows.Forms.MenuStrip MenuStrip;
         private System.Windows.Forms.ToolStripMenuItem TsmiWrite;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel TslWrite;
     }
 }
--- MaiCourseWork/Tables.cs
+++ MaiCourseWork/Tables.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MaiCourseWork
 {
@@ -14,7 +15,7 @@ namespace MaiCourseWork
     {
         int table;
         bool mode;
-        public Tables(int _table,bool _mode)
+        public Tables(int _table, bool _mode)
         {
             InitializeComponent();
 
@@ -43,13 +44,108 @@ namespace MaiCourseWork
             }
             else//мод файла 
             {
-
+                //заполнение таблицы данными из файла
+                switch (table)
+                {
+                    case 0:
+                        if (File.Exists("Purchase.txt"))
+                        {
+                            string[] filetext = File.ReadAllLines("Purchase.txt");
+                            for (int i = 0; i < filetext.Length; i++)
+                            {
+                                string[] text = filetext[i].Split();
+                                DgwData.Rows.Add();
+                                DgwData.Rows[i].Cells[0].Value = text[0];
+                                DgwData.Rows[i].Cells[1].Value = text[1];
+                                DgwData.Rows[i].Cells[2].Value = text[2];
+                                DgwData.Rows[i].Cells[3].Value = text[3];
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Purchase.txt not exist");
+                        }
+                        break;
+                    case 1:
+                        if (File.Exists("Sells.txt"))
+                        {
+                            string[] filetext = File.ReadAllLines("Sells.txt");
+                            for (int i = 0; i < filetext.Length; i++)
+                            {
+                                string[] text = filetext[i].Split();
+                                DgwData.Rows.Add();
+                                DgwData.Rows[i].Cells[0].Value = text[0];
+                                DgwData.Rows[i].Cells[1].Value = text[1];
+                                DgwData.Rows[i].Cells[2].Value = text[2];
+                                DgwData.Rows[i].Cells[3].Value = text[3];
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Sells.txt not exist");
+                        }
+                        break;
+                    case 2:
+                        if (File.Exists("Stock.txt"))
+                        {
+                            string[] filetext = File.ReadAllLines("Stock.txt");
+                            for (int i = 0; i < filetext.Length; i++)
+                            {
+                                string[] text = filetext[i].Split();
+                                DgwData.Rows.Add();
+                                DgwData.Rows[i].Cells[0].Value = text[0];
+                                DgwData.Rows[i].Cells[1].Value = text[1];
+                                DgwData.Rows[i].Cells[2].Value = text[2];
+                                DgwData.Rows[i].Cells[3].Value = text[3];
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Stock.txt not exist");
+                        }
+                        break;
+                }
             }
         }
 
         private void TsmiWrite_Click(object sender, EventArgs e)
         {
-
+            if (mode)//Мод базы данных
+            {
+                
+            }
+            else
+            {
+                StreamWriter sw;
+                switch (table)
+                {
+                    case 0:
+                        sw = new StreamWriter("Purchase.txt");
+                        for (int i = 0; i < DgwData.Rows.Count - 1; i++)
+                        {
+                            sw.WriteLine($"{DgwData.Rows[i].Cells[0].Value} {DgwData.Rows[i].Cells[1].Value} {DgwData.Rows[i].Cells[2].Value} {DgwData.Rows[i].Cells[3].Value}");
+                        }
+                        sw.Close();
+                        break;
+                    case 1:
+                        sw = new StreamWriter("Sells.txt");
+                        for (int i = 0; i < DgwData.Rows.Count - 1; i++)
+                        {
+                            sw.WriteLine($"{DgwData.Rows[i].Cells[0].Value} {DgwData.Rows[i].Cells[1].Value} {DgwData.Rows[i].Cells[2].Value} {DgwData.Rows[i].Cells[3].Value}");
+                        }
+                        sw.Close();
+                        break;
+                    case 2:
+                        sw = new StreamWriter("Stock.txt");
+                        for (int i = 0; i < DgwData.Rows.Count - 1; i++)
+                        {
+                            sw.WriteLine($"{DgwData.Rows[i].Cells[0].Value} {DgwData.Rows[i].Cells[1].Value} {DgwData.Rows[i].Cells[2].Value} {DgwData.Rows[i].Cells[3].Value}");
+                        }
+                        sw.Close();
+                        break;
+                }
+                TslWrite.Text = "Data was updated";
+            }
         }
     }
 }
